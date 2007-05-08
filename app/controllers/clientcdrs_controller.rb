c8d36fdc8f1f104e663479868fd1668175dc392d
simonm <simonm@44406269-ea29-0410-822d-9b5268fb9cc2>
2007-05-08 18:09:50 +0000

    Tweaks
    git-svn-id: https://jump.sla-mobile.com/svn_internal-projects/trunk/pbx-billing@27 44406269-ea29-0410-822d-9b5268fb9cc2

--- app/controllers/clientcdrs_controller.rb
+++ app/controllers/clientcdrs_controller.rb
@@ -1,3 +1,5 @@
+require 'faster_csv'
+
 class ClientcdrsController < ApplicationController
   def index
     list
@@ -64,4 +66,65 @@ class ClientcdrsController < ApplicationController
     Clientcdr.find(params[:id]).destroy
     redirect_to :action => 'list'
   end
+
+  def export_to_excel
+    e = Excel::Workbook.new
+
+    if params[:clientbill_id]
+      @clientbillcdrs = Clientcdr.find(:all, 
+				:conditions => ["clientbill_id = ?", params[:clientbill_id]])
+    else
+      @clientbillcdrs = Clientcdr.find(:all)
+    end
+
+    e.addWorksheetFromActiveRecord "ClientBill", "clientcdr", @clientbillcdrs
+    headers['Content-Type'] = "application/vnd.ms-excel"
+    render_text(e.build)
+  end
+
+
+  def export_to_csv
+
+    if params[:clientbill_id]
+      @clientbillcdrs = Clientcdr.find(:all, 
+				:conditions => ["clientbill_id = ?", params[:clientbill_id]])
+    else
+      @clientbillcdrs = Clientcdr.find(:all)
+    end
+
+    stream_csv do|csv|
+      csv << ["Source", "Destination", "Date", "Cost" ]
+
+      @clientbillcdrs.each do |record|
+        csv << [record['src'],
+                record['dst'],
+                record['dt'],
+                record['cost']]
+      end
+    end
+  end
+
+  private
+    def stream_csv
+       filename = params[:action] + ".csv"    
+
+       #this is required if you want this to work with IE        
+       if request.env['HTTP_USER_AGENT'] =~ /msie/i
+         headers['Pragma'] = 'public'
+         headers["Content-type"] = "text/plain" 
+         headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+         headers['Content-Disposition'] = "attachment; filename=\"#{filename}\"" 
+         headers['Expires'] = "0" 
+       else
+         headers["Content-Type"] ||= 'text/csv'
+         headers["Content-Disposition"] = "attachment; filename=\"#{filename}\"" 
+       end
+
+      render :text => Proc.new { |response, output|
+        csv = FasterCSV.new(output, :row_sep => "\r\n") 
+        yield csv
+      }
+    end
+
+
 end
